4cc936e40b865c513a500872bddae268610af82c
Ngoc Hieu Nguyen <ngochieua2@gmail.com>
2021-05-26 10:16:51 +0700

    add jquery

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// make scroll smooth
+jQuery(document).ready(function($){
+    var $root = $('html, body');
+    $('a[href^="#"]').click(function() {
+        var href = $.attr(this, 'href');
+        $root.animate({
+            scrollTop: $(href).offset().top
+        }, 500, function () {
+            window.location.hash = href;
+        });
+         
+        return false;
+    });
+ 
+})
